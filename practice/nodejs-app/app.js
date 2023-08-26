e324d14d3b7c256bb223511d95700cd4ea26b63c
rishabh0071 <57853470+rishabh0071@users.noreply.github.com>
2023-08-26 16:54:43 +0530

    commit changes

---
+++ practice/nodejs-app/app.js
@@ -0,0 +1,14 @@
+
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/hello.html'));
+    });
+
+    app.listen(8080, () => {
+        console.log('App listening on port 8080');
+        });
